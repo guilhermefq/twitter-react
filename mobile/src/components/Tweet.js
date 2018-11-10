3b5f9fa5f8a4805a2ec333024a59a58aa752a424
Guilherme Fantuci Queiroz <guilhermefq@Guilherme-DSVMac.local>
2018-11-10 18:10:33 -0300

    Finalizaçãão React Native

--- mobile/src/components/Tweet.js
+++ mobile/src/components/Tweet.js
@@ -1,3 +1,35 @@
+import React, { Component } from 'react';
+import api from '../services/api';
+
+import { View, StyleSheet, Text, TouchableOpacity } from 'react-native';
+
+import Icon from 'react-native-vector-icons/Ionicons';
+
+export default class Tweet extends Component {
+  handleLike = async () => {
+    const { _id } = this.props.tweet;
+
+    await api.post(`likes/${_id}`);    
+  };
+
+  render () {
+    const { tweet } = this.props;
+
+    return (
+      <View style={styles.container}>
+        <Text style={styles.author}>{tweet.author}</Text>
+        <Text style={styles.content}>{tweet.content}</Text>
+
+        <TouchableOpacity onPress={this.handleLike} style={styles.likeButton}>
+          <Icon name="ios-heart-empty" size={20} color="#999" />
+          <Text style={styles.likeText}>{tweet.likes}</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }  
+}
+
+
 const styles = StyleSheet.create({
   container: {
     padding: 20,
